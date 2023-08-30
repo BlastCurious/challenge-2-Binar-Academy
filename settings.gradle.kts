b193743e630428ca1e1d8968a4719e242adbf736
Ilyasa Adam N <ilyasagans@gmail.com>
2023-08-30 19:30:28 +0700

    menghapus codingan

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "challenge_2_ilyasa_adam_naufal"
+include(":app")
